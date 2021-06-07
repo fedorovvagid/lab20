4d6fdc7bfe4123cc67b9cb64b5eea92f78980f08
fedorovvagid <75389056+fedorovvagid@users.noreply.github.com>
2021-06-07 23:29:15 +0300

    Add files via upload

---
+++ lab20/src/BaseAuthService.java
@@ -0,0 +1,76 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.SQLException;
+
+public class BaseAuthService implements AuthService {
+    final static String connectionUrl = "jdbc:sqlite:identifier.sqlite";
+    private static Connection connection;
+    private static Statement stmt;
+
+    private class Entry {
+        private String login;
+        private String pass;
+        private String nick;
+
+        public Entry(String login, String pass, String nick) {
+            this.login = login;
+            this.pass = pass;
+            this.nick = nick;
+        }
+    }
+
+    private List<Entry> entries;
+
+    @Override
+
+    public void start() {
+        System.out.println("Сервис аутентификации запущен");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Сервис аутентификации остановлен");
+
+    }
+
+    public BaseAuthService() {
+        entries = new ArrayList<>();
+        entries.add(new Entry("login1", "pass1", "nick1"));
+        entries.add(new Entry("login2", "pass2", "nick2"));
+        entries.add(new Entry("login3", "pass3", "nick3"));
+    }
+
+    @Override
+    public String getNickByLoginPass(String login, String pass) {
+        for (Entry o : entries) {
+            if (o.login.equals(login) && o.pass.equals(pass)) return o.nick;
+        }
+        return null;
+    }
+
+    public void connect() throws SQLException, ClassNotFoundException {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection(connectionUrl);
+        stmt = connection.createStatement();
+    }
+
+    public void closeConnection() {
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
